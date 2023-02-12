4920255eb291d0cdd1dab39e767ecf7540ff318c
傅雨辰 <leiersz@126.com>
2023-02-12 16:40:27 +0800

    implement new rule

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,4 +1,4 @@
-export const hints = ["", "围绕大本营", "被冻结", "周围没有己方格子", "可用"];
+export const hints = ["", "围绕大本营", "被冻结", "周围没有己方格子", "大本营不能回血", "可用"];
 export const classes = [
   "cnone",
   "cpublic",
